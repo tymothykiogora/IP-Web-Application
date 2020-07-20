bba76ffcffe56143651047783871415bd1e8337a
Your Name <you@example.com>
2020-07-21 01:01:33 +0300

    changed background color

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var century, year, month, dayOfMonth, dayOfWeek, day;
 //Get input
       function getInput(){
          century = parseInt(document.getElementById("century").value);
-         year = parseInt(document.getElementById("year").value);
-  month = parseInt(document.getElementById("month").value);
-  dayOfMonth = parseInt(document.getElementById("monthday").value);
+            year = parseInt(document.getElementById("year").value);
+               month = parseInt(document.getElementById("month").value);
+                  dayOfMonth = parseInt(document.getElementById("monthday").value);
 
 
   if(century == ""){
@@ -21,26 +21,27 @@ var century, year, month, dayOfMonth, dayOfWeek, day;
     return false;
   }
 }
+
 //Calculate func
-function calculateDay(){
-    getInput();
-    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
-    console.log(dayOfWeek); //Test the calculateDay function
-    return (Math.floor(dayOfWeek));
-    if (dayOfWeek < 0) {
-      dayOfWeek = dayOfWeek * -1;
-    }
-    else if (dayOfWeek > 0) {
-      return dayOfWeek;
-    }
-}
+      function calculateDay(){
+         getInput();
+            dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
+               console.log(dayOfWeek); //Test the calculateDay function
+                  return (Math.floor(dayOfWeek));
+                      if (dayOfWeek < 0) {
+                     dayOfWeek = dayOfWeek * -1;
+                     }
+                     else if (dayOfWeek > 0) {
+                  return dayOfWeek;
+                     }
+                     }
 
 //main caller func
- function checkDayOfWeek(){
-     day = calculateDay();
-      checkGender();
-      console.log("The function runs");//Test chackDayOfWeek function
-}
+      function checkDayOfWeek(){
+         day = calculateDay();
+            checkGender();
+               console.log("The function runs");//Test chackDayOfWeek function
+                     }
 
 //arrays
 let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
@@ -48,64 +49,78 @@ let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 
 //get selected radio button
-function checkGender(){
-  var gen = document.getElementsByName("rads");
-  if(gen[0].checked == true){
-      var gender = "male";
-  }else if(gen[1].checked == true){
-      var gender = "female";
-  }else {
-    console.log("pass");//Test the radio buttons
+   function checkGender(){
+      var gen = document.getElementsByName("rads");
+         if(gen[0].checked == true){
+            var gender = "male";
+              }else if(gen[1].checked == true){
+            var gender = "female";
+         }else {
+       console.log("pass");//Test the radio buttons
   }
-    switch(gender){
-        case gender = "male":
-              switch(day){
-                case (0 || -0):
-                  document.getElementById("result").innerHTML = "The day is on a sunday." + "  " + "Your akan name is " + maleNames[0];
-                break;
-                case (1 || -1):
+
+     switch(gender){
+           case gender = "male":
+                 switch(day){
+                   case (0 || -0):
+
+                     document.getElementById("result").innerHTML = "The day is on a sunday." + "  " + "Your akan name is " + maleNames[0];
+                      break;
+                  case (1 || -1):
+
                   document.getElementById("result").innerHTML = "The day is on a monday." + " " + "Your akan name is " + maleNames[1];
                 break;
                 case (2 || -2):
+
                   document.getElementById("result").innerHTML = "The day is on a tuesday." + " " + "Your akan name is " + maleNames[2];
                 break;
+
                 case (3 || -3):
                   document.getElementById("result").innerHTML = "The day is on a wednesday." + " " + "Your akan name is "+ maleNames[3];
                 break;
+
                 case (4 || -4):
                   document.getElementById("result").innerHTML = "The day is on a thursday." + " " + "Your akan name is " + maleNames[4];
                 break;
+
                 case (5 || -5):
-                  document.getElementById("result").innerHTML = "The day is on a friday." + " " + "Your akan name is " + maleNames[5];
+               document.getElementById("result").innerHTML = "The day is on a friday." + " " + "Your akan name is " + maleNames[5];
                 break;
+
                 case (6 || -6):
                   document.getElementById("result").innerHTML = "The day is on a saturday." + " " + "Your akan name is " + maleNames[6];
                 break;
                 default:
                 // console.console.log("Pass");//Test male case
               }
-              
+
         break;
         case gender = "female":
                 switch(day){
                   case 0 || -0:
                     document.getElementById("result").innerHTML = "The day is on a sunday." + "  " + "Your akan name is  akosua";
                   break;
+
                   case 1 || -1:
                     document.getElementById("result").innerHTML = "The day is on a monday." + " " + "Your akan name is adwoa ";
                   break;
+
                   case 2 || -2:
                     document.getElementById("result").innerHTML = "The day is on a tuesday." + " " + "Your akan name is abenaa";
                   break;
+
                   case 3 || -3:
                     document.getElementById("result").innerHTML = "The day is on a wednesday." + " " + "Your akan name is akua";
                   break;
+
                   case 4 || -4:
                     document.getElementById("result").innerHTML = "The day is on a thursday." + " " + "Your akan name is yaa";
                   break;
+
                   case 5 || -5:
                     document.getElementById("result").innerHTML = "The day is on a friday." + " " + "Your akan name is afua";
                   break;
+
                   case 6 || -6:
                     document.getElementById("result").innerHTML = "The day is on a saturday." + " " + "Your akan name is ama";
                   break;
